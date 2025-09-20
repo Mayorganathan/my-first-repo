578c00586249756d815db4eb8995d665b69e8df0
Mayorganathan <mayorganathan@gmail.com>
2025-09-19 20:40:57 -0700

    Create Single page CV | roadmap.sh

---
+++ Single page CV | roadmap.sh
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>  
+    <title>CV(Curriculum Vitae)</title>
+    
+</head>
+<body>
+    <h3> Your Name</h3>
+    <p style="color: green;"> Junior Web Developer</p>
+    <p> 5660 Mesagrove ave<br> Norwalk,IL 90640<br> 256-246-5952<br> nathanm@gmail.com</p>
+
+    <h3 style="color: green;"> Skills</h3>
+    <p> HTML,CSS,Javascript,Python,Writing</p>
+
+    <h3 style="color: green;"> Education</h3>
+    <p style="color: #25B8FF;"> Cornell University, New York - Architecture</p>
+    <p>August 2019 to October 2024</p>
+    <p>Exciting Things i did at University</p>
+    <ul>
+        <li> Dragon Day</li>
+        <li> Slope Day</li>
+        <li> Hockey Games</li>
+        <li> Hiking </li>
+        <li> Kayaking</li>
+        <li> Ithaca Farmers</li>
+    </ul>
+    <h3 style="color: green;"> Experience</h3>
+    <p style="color: #25B8FF;">Google, Mountain View,CA - Junior Web Developer</p>
+    <p> October 29th, 2026 - Present</p>
+    <ol>
+        <li> Design User Interface</li>
+        <li> Deploying many Projects</li>
+        <li> Improved many user interface</li>
+        <li> Business Metric</li>
+        <li> Managing Projects</li>
+        <li> Building Strong Teams</li>
+        <li> Becoming Senior Web Developer</li>
+        <p> Skills: Gained from the company</p>
+        <ul>
+            <li> Advanced Front-end and Back-end Tech</li>
+            <li> Complex Database Management</li>
+            <li> DevOps and CI/CD Pipelines</li>
+            <li> Cloud Architecture and Security</li>
+            <li> Large-Scale Project Management</li>
+            <li><span style="color: red;"> Freelancer! </span></li>
+        </ul>
+    </ol>
+    <h3 style="color: green;"> Across the Internet</h3>
+    <a href="https://github.com" target="_blank">GitHub</a>
+    <a href="https://www.linkedin.com" target="_blank">Linkedin</a>
+    <a href="https://www.instagram.com" target="_blank"> Instagram</a>
+</body>
+
+
+
+</html>
